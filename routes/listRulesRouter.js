d577e3acd61dec75dd41c621545aef9913ab9dd2
Ruchi Malik <ruchi@choozle.com>
2019-10-22 18:51:29 -0600

    added logic for paginating clients and rules

--- routes/listRulesRouter.js
+++ routes/listRulesRouter.js
@@ -4,6 +4,62 @@ var router = express.Router();
 
 router.use(express.urlencoded())
 
+/*
+Pagination with
+getRules and getClients, 
+for accounts with many apps and rules
+*/
+
+//getRules is recursive function to iterate through all the rules gotten by the getRulesPage function
+async function getRules(management, pagination) {
+    let allRules = [];
+    var rulePage = await getRulePage(management,pagination)
+    allRules.push( rulePage );
+    console.log( rulePage ) ;
+    if ( rulePage.length > 0 ) {
+        pagination.page = pagination.page + 1;
+        console.log ( 'We got ' + rulePage.length + ' results with pagination of ' + pagination.per_page );
+        allRules.push(await getRules(management, pagination));
+    } else {
+        console.log ( 'We got ' + rulePage.length + ' results with pagination of ' + pagination.per_page );
+        return;
+    }
+    return allRules.flat();
+}
+
+//management is be auth0 client and pagination will be 
+//an associated array of two numbers - rules per page and starting page
+async function getRulePage(management, pagination ) {
+    return await management.getRules(pagination)
+        .then(function(rules) {
+        return rules;
+    })
+}
+
+
+async function getClients(management, pagination) {
+    let allClients = [];
+    var clientPage = await getClientPage(management,pagination)
+    allClients.push( clientPage );
+    console.log( clientPage ) ;
+    if ( clientPage.length > 0 ) {
+        pagination.page = pagination.page + 1;
+        console.log ( 'We got ' + clientPage.length + ' results with pagination of ' + pagination.per_page );
+        allClients.push(await getClients(management, pagination));
+    } else {
+        console.log ( 'We got ' + clientPage.length + ' results with pagination of ' + pagination.per_page );
+    return;
+    }
+return allClients.flat();
+}
+
+async function getClientPage(management, pagination ) {
+    return await management.getClients(pagination).then(function(clients) {
+        return clients;
+    })
+}
+
+
 function getInfo(clients, rules) {
     // Initialize variables!
     var ruleList = [];
@@ -14,10 +70,13 @@ function getInfo(clients, rules) {
 
     // Initialziing html snippets for each app
     for ( var i = 0; i < clients.length; i++ ) {
-        ruleList[ clients[i]['name'] ] = "<b>" + clients[i]['name'] + "</b>"
+        if ( clients[i] != undefined ) {
+            ruleList[ clients[i]['name'] ] = "<b>" + clients[i]['name'] + "</b>"
+        }
     }
 
     for( var i = 0; i < rules.length; i++ ) {
+        if ( rules[i] != undefined ) {
         var match = clientMatch.exec( rules[i]['script'] );
         console.log( rules[i]['name'] )
         if (match != null ) {
@@ -28,6 +87,7 @@ function getInfo(clients, rules) {
                 // We did not find the line in the script for this rule. Assuming this should apply to ALL apps.
                 ruleList[ key ] = ruleList[ key ] + '<br>' + rules[i]['name']
             }
+        }
         }
     }
 
@@ -41,6 +101,7 @@ function getInfo(clients, rules) {
 }
 
 
+
 router.post('/', function (req, res) {
     console.log(req);
     var token = req.body.AccessToken;
@@ -49,20 +110,32 @@ router.post('/', function (req, res) {
         token: token,
         domain: domain
     });
-
-    management
-        .getClients()
-        .then(function(clients) {
-            management
-                .getRules()
-                .then(function(rules) {
-                    var info = getInfo(clients, rules);
-                    res.send('Here are your rules for each app:<br><br>' + info)
-            })
-        })
-        .catch(function(err) {
-            console.log( err);
+    var rule_pagination = {
+        per_page: 2,
+        page: 0
+    };
+    getRules(management, rule_pagination).then(function(rules) {
+        console.log( rules );
+        for( var i = 0; i < rules.length; i++ ) {
+            if( rules[i] != undefined ) {
+                console.log( rules[i]['name']);
+            }
+        }
+        var client_pagination = {
+            per_page: 2,
+            page: 0
+        };
+        getClients(management, client_pagination).then(function(clients) {
+            for( var i = 0; i < clients.length; i++ ) {
+                if( clients[i] != undefined ) {
+                    console.log( clients[i]['name']);
+                }
+            }
+            var output = getInfo(clients, rules );
+            res.send( output); 
         });
-})
+        });
+    })
+
 module.exports = router;
 
